c704a5404a4a138e89b3fb3c7665c9fb8c579990
Marián Ferenc (Markhoss) <114438480+openmarkhoss@users.noreply.github.com>
2025-12-28 05:11:50 +0100

    Create uninstall_eloac.sh

---
+++ uninstall_eloac.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+# ============================================
+# E-L-O-A-C v0.1 Uninstaller
+# Ephemeral Local Offline AI Chat
+# ============================================
+
+echo "======================================"
+echo " E-L-O-A-C v0.1 UNINSTALLER"
+echo "======================================"
+echo
+echo "To uninstall, type the following"
+echo "on your keyboard and press Enter:"
+echo
+echo "  rm -rf ~/eloac"
+echo
+echo "That is all."
+echo
